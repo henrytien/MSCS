645d884f4eab6c2b2983286139e64f122d2803ed
henrytine <tianhangyu8886@gmail.com>
2020-08-26 20:22:07 +0800

    Update seqlist.cpp

--- Docs/data_structure/src/chap02/seqlist.cpp
+++ Docs/data_structure/src/chap02/seqlist.cpp
@@ -30,10 +30,27 @@ void IncreaseSize(SeqList &L, int len) {
     L.MaxSize = L.MaxSize + len;
     free(p);
 }
+
+// 线性表的插入
+bool ListInsert(SeqList &L, int i, int e) {
+    if (i < 1 || i > L.length + 1)
+        return false;
+    if (i >= L.MaxSize)
+        return false;
+    for (int j = L.length; j >= i; --j) {
+        L.data[j-1] = L.data[j];
+    }
+    L.data[i-1] = e;
+    L.length++;
+}
 int main () {
     SeqList L;
     InitList(L);
     IncreaseSize(L, 10);
+
+    for (int i = 0; i < 10; ++i) {
+        ListInsert(L, 0, i);
+    }
 
     return 0;
 }
